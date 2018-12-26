c8fa3be6555233b53497c375ddd48e96da95541e
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2018-12-26 21:52:45 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,18 @@
+# A simple fireplace in kakoune
+
+Based on [this fireplace](https://medium.com/sweetmeat/python-curses-based-ascii-art-fire-animation-259e9e007767)
+
+Inspired by [Emacs fireplace](https://github.com/johanvts/emacs-fireplace)
+
+## Installation
+
+- Symlink `fireplace.py` to `%val{config}/bin/`
+- Let kakoune load `fireplace.kak`
+
+## Usage
+
+Run `:fireplace`
+
+Scroll to the bottom of the file (<key>gj</key>)
+
+You are suggested to disable line wrapping, line numbers and scrolloff for the best results.
---
+++ fireplace.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import random
+import time
+import os
+import sys
+
+width = int(os.environ['kak_window_width'])
+height = int(os.environ['kak_window_height'])
+size = width * height
+char = [" ", ".", ":", "^", "*", "x", "s", "S", "#", "$"]
+b = []
+
+for i in range(size + width + 1):
+    b.append(0)
+f = os.open(sys.argv[1], os.O_WRONLY)
+while 1:
+    for i in range(int(width / 9)):
+        b[int((random.random() * width) + width * (height - 1))] = 65
+    for i in range(size):
+        b[i] = int((b[i] + b[i + 1] + b[i + width] + b[i + width + 1]) / 4)
+        color = (4 if b[i] > 15 else (3 if b[i] > 9 else
+                                      (2 if b[i] > 4 else 1)))
+    string = ""
+    for i in range(int(len(b) / width)):
+        string += ''.join([char[(9 if x > 9 else x)] for x in b[i*width:(i+1)*width]])
+        string += "\n"
+    os.write(f,bytes(string.encode('utf-8')))
+    time.sleep(0.03)
